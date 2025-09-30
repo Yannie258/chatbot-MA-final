eb63528c44f25d5e498a0d36e494cf9d8b65ef60
Yen Nguyen <yen_ngoc_289@yahoo.com>
2025-10-01 00:50:40 +0200

    split plain version

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         <Header />
         <MainSessions />
         <Footer />
-         <ChatBot apiUrl={apiUrl} /> 
-        {/* <PlainChatBot apiUrl={apiUrl} /> */}
+         {/* <ChatBot apiUrl={apiUrl} />  */}
+        <PlainChatBot apiUrl={apiUrl} /> 
       </div>
     </>
   );
